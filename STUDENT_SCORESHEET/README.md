054a366de40f4efe098b7f12fc8c03eee5b5f500
Samuel Kuria <iamsamuelkuria@gmail.com>
2025-12-05 17:26:02 +0300

    planning

---
+++ STUDENT_SCORESHEET/README.md
@@ -0,0 +1,3 @@
+## Student Scoresheet
+
+![Desired output](image.png)
---
+++ STUDENT_SCORESHEET/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Scanner to get at least 12 inputs
+        //variales: School Name, Teacher Name, Grade, Year
+        //grading system: use switch case
+        //compute: total marks, average and grade counts for class(Grade)
+        //compute: total each student, rank(grade)
+        //Remember: error handling eg invalid grade, invalid input type, blank input 
+        System.out.println("STUDENT SCORESHEET");
+    }
+}
